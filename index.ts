7ef78da2f21bf5e375fdd0af486bb0118b6dfdf9
rudxor02 <rudxor07@naver.com>
2023-04-22 14:21:51 +0900

    feat: build test env

--- index.ts
+++ index.ts
@@ -1,1 +1,28 @@
-console.log("Hello World");
+import "reflect-metadata";
+import {
+  EmptyClass,
+  EmptyParam,
+  INJECTABLE_WATERMARK,
+  Injectable,
+  InjectableTest,
+  PARAMTYPES_METADATA,
+  SCOPE_OPTIONS_METADATA,
+  SELF_DECLARED_DEPS_METADATA,
+} from "./nest";
+
+class B {}
+
+@EmptyClass()
+class A {
+  constructor(
+    public readonly a: B,
+    @EmptyParam()
+    public readonly b = 3
+  ) {}
+}
+
+// Reflect.defineMetadata(INJECTABLE_WATERMARK, true, A);
+// console.log(Reflect.getMetadata(INJECTABLE_WATERMARK, A));
+console.log(Reflect.getMetadataKeys(A));
+console.log(Reflect.getMetadataKeys(B));
+// console.log(Reflect.getMetadata("test", A));
